aabd29dbccf3ec185a04d384f6f306a1bb3b3133
paula <ppintonunez@gmail.com>
2024-04-13 16:46:15 +0200

    ok v7 arreglados echos de index.php

--- app/index.php
+++ app/index.php
@@ -1,26 +1,5 @@
 <?php
-// phpinfo();
-$hostname = "db";
-$username = "admin";
-$password = "test";
-$db = "database";
-
-$conn = mysqli_connect($hostname, $username, $password, $db);
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-
-$query = mysqli_query($conn, "SELECT * FROM usuarios") or die(mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-    echo
-        "<tr>
-    <td>{$row['username']}</td>
-    <td>{$row['password']}</td>
-    <td>{$row['name']}</td>
-    <td>{$row['email']}</td>
-   </tr>";
-}
+// index.php
 
 // Obtener solicitud y parametros
 $method = $_SERVER['REQUEST_METHOD'];
